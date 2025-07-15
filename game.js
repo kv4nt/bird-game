84e3c313be6906c5e2bc65ef32e5dad9f90c6564
shumeikov <viktor.shumeiko@simbirsoft.com>
2025-07-15 21:27:52 +0300

    init

--- game.js
+++ game.js
@@ -55,6 +55,7 @@ function checkCollisions() {
 
     for (let p of pipes) {
         // Проверяем столкновение с верхней трубой
+        console.log(p.y,bird.y);
         if (bird.x > p.x && bird.x < p.x && bird.y < p.y + pg) {
             console.log('Верхняя труба');
             return true;
