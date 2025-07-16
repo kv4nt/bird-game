5b72658d28f4da0e900c6bb4e492870aa987e150
shumeikov <viktor.shumeiko@simbirsoft.com>
2025-07-16 09:42:33 +0300

    init

--- js/game.js
+++ js/game.js
@@ -445,7 +445,7 @@ function makePipes(scene) {
 
     const pipeTopY = Phaser.Math.Between(-120, 120)
 
-    const gap = scene.add.line(288, pipeTopY + 510, 0, 0, 0, 98)
+    const gap = scene.add.line(288, pipeTopY + 210, 0, 0, 0, 198)
     gapsGroup.add(gap)
     gap.body.allowGravity = false
     gap.visible = false
