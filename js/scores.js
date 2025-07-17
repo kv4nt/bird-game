b6625bef59e398fed469b536cc6c8bb48170b5e2
shumeikov <viktor.shumeiko@simbirsoft.com>
2025-07-17 19:20:45 +0300

    init

--- js/scores.js
+++ js/scores.js
@@ -26,7 +26,7 @@ class Highscore extends Phaser.Scene {
         this.add.bitmapText(10, 20, 'knighthawks', 'RANK  SCORE   NAME   DATE').setTint(0xffffff);
         for (let i = 1; i < 11; i++) {
             if (scores[i-1]) {
-                this.add.bitmapText(10, 50 * i, 'knighthawks', ` ${i}      ${scores[i-1].score}    ${scores[i-1].user_name}  ${scores[i-1].date}`).setTint(0xffffff);
+                this.add.bitmapText(10, 10 * i, 'knighthawks', ` ${i}      ${scores[i-1].score}    ${scores[i-1].user_name}  ${scores[i-1].date}`).setTint(0xffffff);
             } else {
                 this.add.bitmapText(10, 50 * i, 'knighthawks', ` ${i}      0    ---`).setTint(0xffffff);
             }
