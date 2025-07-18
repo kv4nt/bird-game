27b07598621d00c327c2afa3c7fd91b2d6e1fefe
shumeikov <viktor.shumeiko@simbirsoft.com>
2025-07-18 12:04:42 +0300

    init

--- js/game.js
+++ js/game.js
@@ -223,6 +223,64 @@ let score
  *   Load the game assets.
  */
 function preload() {
+    var progressBar = this.add.graphics();
+    var progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(240, 270, 320, 50);
+
+    var width = this.cameras.main.width;
+    var height = this.cameras.main.height;
+    var loadingText = this.make.text({
+        x: width / 2,
+        y: height / 2 - 50,
+        text: 'Loading...',
+        style: {
+            font: '20px monospace',
+            fill: '#ffffff'
+        }
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    var percentText = this.make.text({
+        x: width / 2,
+        y: height / 2 - 5,
+        text: '0%',
+        style: {
+            font: '18px monospace',
+            fill: '#ffffff'
+        }
+    });
+    percentText.setOrigin(0.5, 0.5);
+
+    var assetText = this.make.text({
+        x: width / 2,
+        y: height / 2 + 50,
+        text: '',
+        style: {
+            font: '18px monospace',
+            fill: '#ffffff'
+        }
+    });
+    assetText.setOrigin(0.5, 0.5);
+
+    this.load.on('progress', function (value) {
+        percentText.setText(parseInt(value * 100) + '%');
+        progressBar.clear();
+        progressBar.fillStyle(0xffffff, 1);
+        progressBar.fillRect(250, 280, 300 * value, 30);
+    });
+
+    this.load.on('fileprogress', function (file) {
+        assetText.setText('Loading asset: ' + file.key);
+    });
+
+    this.load.on('complete', function () {
+        progressBar.destroy();
+        progressBox.destroy();
+        loadingText.destroy();
+        percentText.destroy();
+        assetText.destroy();
+    });
     // Backgrounds and ground
     this.load.image(assets.scene.background.day, 'assets/background-day.png')
     this.load.image(assets.scene.background.night, 'assets/background-night.png')
@@ -291,25 +349,6 @@ function preload() {
     this.load.audio('fartSoundTriple', 'sound/fart-triple.wav');
     this.load.audio('bgSound', 'sound/bg.wav');
     this.load.audio('bgSoundRetro', 'sound/bg-retro.wav');
-
-
-    const progress = this.add.graphics();
-
-    this.load.on('progress', value =>
-    {
-
-        progress.clear();
-        progress.fillStyle(0xda2da2, 1);
-        progress.fillRect(0, 270, 800 * value, 60);
-
-    });
-
-    this.load.on('complete', () =>
-    {
-
-        progress.destroy();
-
-    });
 }
 
 /**
